1c0b0f3d7fd0e24090105332db71d6ee7bb74cd1
Tomáš Hobza <hobza.tomas@gmail.com>
2026-06-17 00:05:20 +0200

    formatting: clean up whitespace in Decision class

--- decision.py
+++ decision.py
@@ -4,9 +4,9 @@ class Decision():
   def __init__(self, description, tags: list[TagEnum]):
     self.description = description
     self.tags = tags
-    
+
   def get_tags(self):
     return self.tags
-  
+
   def __str__(self):
     return f"{self.description}\nTags: {', '.join([tag.value for tag in self.tags])}"
